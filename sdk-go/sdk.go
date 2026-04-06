97241ae06b80e159f0fed7ffeeba1ac873e11000
kevburnsjr <kevburnsjr@gmail.com>
2026-04-06 07:17:50 -0700

    init

--- sdk-go/sdk.go
+++ sdk-go/sdk.go
@@ -13,6 +13,11 @@ func (s Shard) Apply(cmd []byte) (val uint64, res []byte, err error) {
 	return
 }
 
+// ShardFind returns the first shard with a name less than or equal to the supplied name
+// This is useful for exact matches but also for partitioning. Example:
+//
+//	shard := cluster.ShardFind(`data-`+sha1(key))
+//	val, res, err := shard.Read(key, true)
 func ShardFind(name string) Shard {
 	return Shard{ID: 1, Name: name}
 }
